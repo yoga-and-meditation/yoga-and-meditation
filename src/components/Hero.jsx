acd9720dc72399bdb34d60c4cf8c1d09feb122c9
Roshini Muralidharan <Roshinipraveen18@gmail.com>
2023-07-13 17:10:47 +0200

    hero basic structure

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,15 @@
+import name from '../assets/images/name.png'
+import gif1 from '../assets/images/gif1.png'
 function Hero() {
     return (
       <div className='hero'> 
+      <div className='hero-words'>
        <h1 className='hero-heading'>Yoga</h1>
- 
+       <img src={name} className="name"/></div>
+       <div className='ball'>
+     </div>
+     <div><img src={gif1} className='gif'></img></div>
+       
       </div>
     )
   }
