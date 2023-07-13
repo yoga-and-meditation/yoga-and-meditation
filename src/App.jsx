76f5db75a58e859613793f77a93a6af745d42cf1
Shaqayeq-taheri <shaqayeq.taheritizro@dci.education>
2023-07-13 12:39:12 +0200

    about me is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/App.scss";
+import Aboutme from './components/Aboutme.jsx';
 
 function App() {
   return (
-    <>
-      <h1>Test</h1>
-      <button>Click Me</button>
-    </>
+    <div className="app">
+      <Aboutme/>
+    </div>
   );
 }
 
--- src/components/Aboutme.jsx
+++ src/components/Aboutme.jsx
@@ -1,8 +1,29 @@
+import sheetalImg from "../assets/images/sheetal.jpg";
 
 function Aboutme() {
   return (
-    <div>Aboutme</div>
-  )
+    <div className="about-container">
+      <img src={sheetalImg} alt="personal photo" />
+      <section>
+        <h1>About Me</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos tenetur
+          ratione ut? Illo odit 
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae
+          esse dolores aperiam doloremque vero, unde quo illum minus quisquam
+          dignissimos aspernatur iste. Perferendis labore quidem necessitatibus?
+          Ab, aut. Iure, atque. Laboriosam, impedit amet architecto eaque,
+          dolore voluptates placeat magni ducimus eius dolores modi sint culpa
+          fuga. Ab distinctio debitis aut libero excepturi, porro unde laborum
+          exercitationem molestiae delectus, quis commodi similique tempore. Eum
+          reprehenderit sapiente quasi provident porro optio tempore pariatur
+          est modi ad! Perferendis iste facere, similique inventore eveniet odio
+          soluta neque ea fuga dolorum! Quia aliquid alias corporis neque minima
+          fuga voluptate numquam, molestias hic laborum quasi omnis!
+        </p>
+      </section>
+    </div>
+  );
 }
 
-export default Aboutme
+export default Aboutme;
