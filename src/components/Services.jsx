8412b240b748520372af8a2d7f8bbbfc480f2d5f
Abdulwase Naeemi <dci-admin@abdulwase-naeemi.local>
2023-07-13 08:50:29 +0200

    some changes has been made

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,8 +1,13 @@
+const Yoga = () => {
+  return <h1>I am yoga</h1>;
+};
+
 function Services() {
   return (
     <div className="services-container">
       <h2>What We Do?</h2>
       <h3>Our Services</h3>
+      <Yoga />
     </div>
   );
 }
