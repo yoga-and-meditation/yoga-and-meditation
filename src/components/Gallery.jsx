c2720b77f7675cc2edf919329854609ae532d936
louisclarencepeter <louisclarencepeters@gmail.com>
2023-07-13 09:23:35 +0200

    created 16 img elements

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -5,7 +5,7 @@ function Gallery() {
       <h2>My Gallery</h2>
 
       <div>
-        
+        <img src="" alt="" /><img src="" alt="" /><img src="" alt="" /><img src="" alt="" /><img src="" alt="" /><img src="" alt="" /><img src="" alt="" /><img src="" alt="" /><img src="" alt="" /><img src="" alt="" /><img src="" alt="" /><img src="" alt="" /><img src="" alt="" /><img src="" alt="" /><img src="" alt="" /><img src="" alt="" />
       </div>
 
     </div>
