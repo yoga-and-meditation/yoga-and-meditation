37dd08473b890fd5b94824496ad81db2abe1d063
Abdulwase Naeemi <dci-admin@abdulwase-naeemi.local>
2023-07-13 10:00:10 +0200

    some clean-up has been made in development branch

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -1,20 +1,5 @@
 function Feedback() {
-  return (
-    <div className="feedback">
-      <h2>Customer Feedback</h2>
-      <div className="feedback-card">
-        <div className="feedback-rating">
-          <span className="rating">4.5</span>
-          <span className="stars">&#9733;&#9733;&#9733;&#9733;&#9734;</span>
-        </div>
-        <p className="feedback-text">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-          vestibulum condimentum mauris nec ultricies.
-        </p>
-        <p className="feedback-author">- John Doe</p>
-      </div>
-    </div>
-  );
+  return <h1>I am Feedback</h1>;
 }
 
 export default Feedback;
