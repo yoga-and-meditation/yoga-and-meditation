98ccc6aaaf52d72b02b4b9c0b96aaec232e60e6b
Shaqayeq-taheri <shaqayeq.taheritizro@dci.education>
2023-07-13 21:47:51 +0200

    update styles

--- src/components/Aboutme.jsx
+++ src/components/Aboutme.jsx
@@ -1,17 +1,15 @@
-import sheetalImg from "../assets/images/Rectangle 593.jpg";
-//import goldRectangle from "../assets/images/Rectangle 594.jpg";
-//import grayRectangle from "../assets/images/Rectangle 595.jpg";
+import sheetalImg from "../assets/images/Rectangle_593-removebg-preview (1).png";
+
 
 function Aboutme() {
   return (
     <div className="about-container">
       <div className="about-img-container">
-        <img src={sheetalImg} alt="personal photo" />
-        {/* <img src={goldRectangle} alt="a gold rectangle"/>
-        <img src={grayRectangle} alt="a gray rectangle" /> */}
+      <img src={sheetalImg} alt="personal photo" /> 
+     
       </div>
       <section>
-        <h1>About Me</h1>
+        <h2>About Me</h2>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente animi voluptatum voluptatem, facilis, fugiat quam placeat consequatur id eveniet pariatur labore, quo quia fuga dignissimos perspiciatis. Eius fuga delectus recusandae.
         </p>
