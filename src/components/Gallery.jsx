25c0dab36e8787586f281f3c7a41b822607edd01
louisclarencepeter <louisclarencepeters@gmail.com>
2023-07-13 13:38:46 +0200

    I add more details in the array to include 'alt'

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,3 +1,4 @@
+// Import all images
 import image1 from '../assets/images/gallery/1.jpg'
 import image2 from '../assets/images/gallery/2.jpg'
 import image3 from '../assets/images/gallery/3.jpg'
@@ -17,39 +18,40 @@ import image16 from '../assets/images/gallery/16.jpg'
 import image17 from '../assets/images/gallery/17.jpg'
 import image18 from '../assets/images/gallery/18.jpg'
 
+// Combine all images into an array
+const galleryImages = [
+  { id: 1, src: image1, alt: 'Image 1' },
+  { id: 2, src: image2, alt: 'Image 2' },
+  { id: 3, src: image3, alt: 'Image 3' },
+  { id: 4, src: image4, alt: 'Image 4' },
+  { id: 5, src: image5, alt: 'Image 5' },
+  { id: 6, src: image6, alt: 'Image 6' },
+  { id: 7, src: image7, alt: 'Image 7' },
+  { id: 8, src: image8, alt: 'Image 8' },
+  { id: 9, src: image9, alt: 'Image 9' },
+  { id: 10, src: image10, alt: 'Image 10' },
+  { id: 11, src: image11, alt: 'Image 11' },
+  { id: 12, src: image12, alt: 'Image 12' },
+  { id: 13, src: image13, alt: 'Image 13' },
+  { id: 14, src: image14, alt: 'Image 14' },
+  { id: 15, src: image15, alt: 'Image 15' },
+  { id: 16, src: image16, alt: 'Image 16' },
+  { id: 17, src: image17, alt: 'Image 17' },
+  { id: 18, src: image18, alt: 'Image 18' },
+];
 
 function Gallery() {
-  const galleryImages = [
-    image1,
-    image2,
-    image3,
-    image4,
-    image5,
-    image6,
-    image7,
-    image8,
-    image9,
-    image10,
-    image11,
-    image12,
-    image13,
-    image14,
-    image15,
-    image16,
-    image17,
-    image18
-  ]
-
   return (
     <div className='gallery-container'>
       <h2>My Gallery</h2>
       <div className='gallery-images-container'>
-      {galleryImages.map((src, index) => (
-        <img key={index} src={src} alt={`Image ${index + 1}`} />
-      ))}
+        {galleryImages.map((image) => (
+          <img key={image.id} src={image.src} alt={image.alt} />
+        ))}
       </div>
     </div>
   );
 }
 
 export default Gallery;
+
