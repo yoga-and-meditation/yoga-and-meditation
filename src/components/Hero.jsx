cdc0fb42e55bc494a31210ce14759e6493982ec0
Roshini Muralidharan <Roshinipraveen18@gmail.com>
2023-07-14 10:19:25 +0200

    made the hero responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,14 +2,14 @@ import name from '../assets/images/name.png'
 import gif1 from '../assets/images/gif1.png'
 function Hero() {
     return (
-      <div className='hero'> 
-      <div className='hero-words'>
+<div >
+  <div className='hero' ></div>
+      <div >
        <h1 className='hero-heading'>Yoga</h1>
        <img src={name} className="name"/></div>
-       <div className='ball'>
+       <div className='ball'><img src={gif1} className='gif'></img>
      </div>
-     <div><img src={gif1} className='gif'></img></div>
-       
+ 
       </div>
     )
   }
