e4c0b649ce0baff00fc6ccd37dc04d7d35883531
Abdulwase Naeemi <dci-admin@abdulwase-naeemi.local>
2023-07-14 10:32:44 +0200

    box-shadow has been added to the card, and the react routering has been changed with the tab

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,106 +1,130 @@
-import { Link, Outlet, BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
 
 import image1 from "../assets/images/services/yoga.jpg";
 import image2 from "../assets/images/services/meditation.jpg";
 import image3 from "../assets/images/services/retreating.jpg";
 
-const cardsData = [
-  {
-    title: "Yoga",
-    id: 1,
-    image: image1,
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam hic provident at unde porro odio voluptas repellat soluta! Ducimus voluptatem vero odio voluptatibus fugiat soluta asperiores, ullam reiciendis vel hic culpa architecto. Perferendis ducimus accusantium perspiciatis qui, accusamus libero earum laudantium maiores voluptatem officiis! Vel quos aliquam tenetur a possimus! asperiores, ullam reiciendis vel hic culpa architecto. Perferendis ducimus accusantium perspiciatis qui, accusamus libero earum laudantium maiores voluptatem officiis! Vel quos aliquam tenetur a possimus!",
-  },
-  {
-    title: "Meditation",
-    id: 2,
-    image: image2,
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam hic provident at unde porro odio voluptas repellat soluta! Ducimus voluptatem vero odio voluptatibus fugiat soluta asperiores, ullam reiciendis vel hic culpa architecto. Perferendis ducimus accusantium perspiciatis qui, accusamus libero earum laudantium maiores voluptatem officiis! Vel quos aliquam tenetur a possimus! asperiores, ullam reiciendis vel hic culpa architecto. Perferendis ducimus accusantium perspiciatis qui, accusamus libero earum laudantium maiores voluptatem officiis! Vel quos aliquam tenetur a possimus!",
-  },
-  {
-    title: "Retreating",
-    id: 3,
-    image: image3,
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam hic provident at unde porro odio voluptas repellat soluta! Ducimus voluptatem vero odio voluptatibus fugiat soluta asperiores, ullam reiciendis vel hic culpa architecto. Perferendis ducimus accusantium perspiciatis qui, accusamus libero earum laudantium maiores voluptatem officiis! Vel quos aliquam tenetur a possimus! asperiores, ullam reiciendis vel hic culpa architecto. Perferendis ducimus accusantium perspiciatis qui, accusamus libero earum laudantium maiores voluptatem officiis! Vel quos aliquam tenetur a possimus!",
-  },
-];
+function Services() {
+  const [tab, setTab] = useState(1);
 
-const ServiceCards = ({ cards }) => {
   return (
-    <div className="service-card">
-      {cards.map((card) => (
-        <div key={card.id}>
-          <div className="card m-5">
+    <div className="service-container">
+      <div className="header-container">
+        <h2>What We Do?</h2>
+        <h3>Our Services</h3>
+        <header>
+          <button className="service-button" onClick={() => setTab(1)}>
+            Yoga
+          </button>
+          <button onClick={() => setTab(2)}>Meditation</button>
+          <button onClick={() => setTab(3)}>Retreats</button>
+        </header>
+      </div>
+      {tab === 1 && (
+        <section>
+          <div className="card m-5 card-container">
             <div className="row g-10">
               <div className="col-md-4">
                 <img
-                  src={card.image}
+                  src={image1}
                   className="img-fluid rounded-start"
-                  alt={card.title}
+                  alt="Yoga image"
                 />
               </div>
               <div className="col-md-8">
                 <div className="card-body">
-                  <h3 className="card-title">{card.title}</h3>
-                  <p className="card-text">{card.description}</p>
-                  <button className="youbtn">Make an Appointment</button>
+                  <h3 className="card-title">Yoga</h3>
+                  <p className="card-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Veniam hic provident at unde porro odio voluptas repellat
+                    soluta! Ducimus voluptatem vero odio voluptatibus fugiat
+                    soluta asperiores, ullam reiciendis vel hic culpa
+                    architecto. Perferendis ducimus accusantium perspiciatis
+                    qui, accusamus libero earum laudantium maiores voluptatem
+                    officiis! Vel quos aliquam tenetur a possimus! asperiores,
+                    ullam reiciendis vel hic culpa architecto. Perferendis
+                    ducimus accusantium perspiciatis qui, accusamus libero earum
+                    laudantium maiores voluptatem officiis! Vel quos aliquam
+                    tenetur a possimus!
+                  </p>
+
+                  <button className="subscribe-btn">Make an Appointment</button>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      ))}
-    </div>
-  );
-};
+        </section>
+      )}
+      {tab === 2 && (
+        <section>
+          <div className="card m-5 card-container">
+            <div className="row g-10">
+              <div className="col-md-4">
+                <img
+                  src={image2}
+                  className="img-fluid rounded-start"
+                  alt="Meditation image"
+                />
+              </div>
+              <div className="col-md-8">
+                <div className="card-body">
+                  <h3 className="card-title">Meditation</h3>
+                  <p className="card-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Veniam hic provident at unde porro odio voluptas repellat
+                    soluta! Ducimus voluptatem vero odio voluptatibus fugiat
+                    soluta asperiores, ullam reiciendis vel hic culpa
+                    architecto. Perferendis ducimus accusantium perspiciatis
+                    qui, accusamus libero earum laudantium maiores voluptatem
+                    officiis! Vel quos aliquam tenetur a possimus! asperiores,
+                    ullam reiciendis vel hic culpa architecto. Perferendis
+                    ducimus accusantium perspiciatis qui, accusamus libero earum
+                    laudantium maiores voluptatem officiis! Vel quos aliquam
+                    tenetur a possimus!
+                  </p>
 
-const Yoga = () => {
-  return <ServiceCards cards={[cardsData[0]]} />;
-};
+                  <button className="subscribe-btn">Make an Appointment</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      )}
+      {tab === 3 && (
+        <section>
+          <div className="card m-5 card-container">
+            <div className="row g-10">
+              <div className="col-md-4">
+                <img
+                  src={image3}
+                  className="img-fluid rounded-start"
+                  alt="Retreats image"
+                />
+              </div>
+              <div className="col-md-8">
+                <div className="card-body">
+                  <h3 className="card-title">Retreats</h3>
+                  <p className="card-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Veniam hic provident at unde porro odio voluptas repellat
+                    soluta! Ducimus voluptatem vero odio voluptatibus fugiat
+                    soluta asperiores, ullam reiciendis vel hic culpa
+                    architecto. Perferendis ducimus accusantium perspiciatis
+                    qui, accusamus libero earum laudantium maiores voluptatem
+                    officiis! Vel quos aliquam tenetur a possimus! asperiores,
+                    ullam reiciendis vel hic culpa architecto. Perferendis
+                    ducimus accusantium perspiciatis qui, accusamus libero earum
+                    laudantium maiores voluptatem officiis! Vel quos aliquam
+                    tenetur a possimus!
+                  </p>
 
-const Meditation = () => {
-  return <ServiceCards cards={[cardsData[1]]} />;
-};
-
-const Retreats = () => {
-  return <ServiceCards cards={[cardsData[2]]} />;
-};
-
-const Parent = () => {
-  return (
-    <div className="parent">
-      <Link to="/">
-        <button>Yoga</button>
-      </Link>
-      <Link to="/meditation">
-        <button>Meditation</button>
-      </Link>
-      <Link to="/retreats">
-        <button>Retreats</button>
-      </Link>
-      <Outlet />
-    </div>
-  );
-};
-
-function Services() {
-  return (
-    <div className="services-container">
-      <h2>What We Do?</h2>
-      <h3>Our Services</h3>
-
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Parent />}>
-            <Route index element={<Yoga />} />
-            <Route path="/meditation" element={<Meditation />} />
-            <Route path="/retreats" element={<Retreats />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+                  <button className="subscribe-btn">Make an Appointment</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      )}
     </div>
   );
 }
