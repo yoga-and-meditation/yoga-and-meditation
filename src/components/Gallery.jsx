8e410faeee9cf121df80e1d54c0a241a7a669b89
Louis Peter <louisclarencepeters@gmail.com>
2023-07-15 15:30:36 +0200

    added comments to my code

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,19 +1,26 @@
 import React from "react";
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 
+// This is the state of the component.
+// It contains a boolean flag that indicates whether the images have been loaded,
+// and an array of objects that represent the images.
 class Gallery extends React.Component {
   state = {
     imagesLoaded: false,
     galleryImages: [],
   };
 
+  // This function is called when the component mounts.
+  // It imports the images and sets the state of the component.
   async componentDidMount() {
     const importedImages = [];
 
     for (let i = 1; i <= 18; i++) {
+      // This code imports the image file.
       const imagePath = `../assets/images/gallery/${i}.jpg`;
       const importedImage = await import(imagePath);
 
+      // This code creates an object that represents the image.
       importedImages.push({
         id: i,
         src: importedImage.default,
@@ -21,6 +28,7 @@ class Gallery extends React.Component {
       });
     }
 
+    // This code sets the state of the component.
     setTimeout(() => {
       this.setState({
         imagesLoaded: true,
@@ -29,6 +37,7 @@ class Gallery extends React.Component {
     }, 500);
   }
 
+  // This function renders the component.
   render() {
     const { imagesLoaded, galleryImages } = this.state;
 
@@ -36,6 +45,7 @@ class Gallery extends React.Component {
       <div className="gallery-container">
         <h2>My Gallery</h2>
         {imagesLoaded && (
+          // This code renders the ResponsiveMasonry component.
           <ResponsiveMasonry
             className="gallery-masonry"
             columnsCountBreakPoints={{
@@ -47,6 +57,7 @@ class Gallery extends React.Component {
           >
             <Masonry className="gallery-images-container">
               {galleryImages.map((image) => (
+                // This code renders an image.
                 <img
                   className="gallery-image"
                   key={image.id}
@@ -62,4 +73,5 @@ class Gallery extends React.Component {
   }
 }
 
+// This exports the component.
 export default Gallery;
