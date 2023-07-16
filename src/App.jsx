b12e67b7bbe8adb26900db61b8efedf3e52b440b
kidan-tekelearegy <kidanlakew.tekelearegay@dci.education>
2023-07-16 11:33:31 +0200

    Icons added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
-
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/App.scss";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
-
 
 import Footer from "./components/Footer";
-
 
 function App() {
   return (
     <>
-      <Footer/>
-     
+      <Footer />
     </>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,49 +1,54 @@
-/* import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faFacebook,
-  faTwitter,
-  faInstagram,
- 
-} from "@fortawesome/free-brands-svg-icons"; */
+//Icons
+import {  FaFacebook,FaInstagram, FaTwitter } from 'react-icons/fa';
 function Footer() {
   return (
-    <div className="footer-container  d-flex">
-      <div className="logo m-5">
-        <h4>logo</h4>
-      </div>
-      <div className="about-me">
-        <h4>About me </h4>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
-          quidem pariatur autem neque, voluptatum sit error sint. Qui, facilis.
-          Consectetur temporibus ullam neque tempore, ut perferendis nostrum
-          necessitatibus repellendus fuga.
-        </p>
-        {/* <div>
+    <>
+      <div className="footer-container  d-flex">
+        <div className="logo m-5">
+          <h4>logo</h4>
+        </div>
+        <div className="about-me">
+          <h4>About me </h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
+            quidem pariatur autem neque, voluptatum sit error sint. Qui,
+            facilis. Consectetur temporibus ullam neque tempore, ut perferendis
+            nostrum necessitatibus repellendus fuga.
+          </p>
+          <div className='icons'>  <FaFacebook />
+           <FaInstagram/>
+           <FaTwitter/>
+           </div>
+       
+          {/* <div>
         <FontAwesomeIcon icon={faFacebook} />
         <FontAwesomeIcon icon={faInstagram} />
         <FontAwesomeIcon icon={faTwitter} />
         </div> */}
-      </div>
+        </div>
 
-      <div className="links">
-        <h4 className="text-center">Links</h4>
-        <ul>
-          <li className="list-group">
-            <a href=""></a>Home
-          </li>
-          <li className="list-group">
-            <a href="#"></a>About Me
-          </li>
-          <li className="list-group">
-            <a href="#"></a>Services
-          </li>
-          <li className="list-group">
-            <a href="#"></a>Contact
-          </li>
-        </ul>
+        <div className="links">
+          <h4 className="text-center">Links</h4>
+          <ul>
+            <li className="list-group">
+              <a href=""></a>Home
+            </li>
+            <li className="list-group">
+              <a href="#"></a>About Me
+            </li>
+            <li className="list-group">
+              <a href="#"></a>Services
+            </li>
+            <li className="list-group">
+              <a href="#"></a>Contact
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
+      <div className="copyright p-3 fixed-bottom">
+        © 2023 Copyright Reserved
+      </div>
+    </>
   );
 }
 
