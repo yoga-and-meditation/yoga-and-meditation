8109befd6b43e0f7bff381cc6adc4e673ef11568
Louis Peter <louisclarencepeters@gmail.com>
2023-07-17 11:18:59 +0200

    added a circle div

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -3,19 +3,22 @@ import image from '../assets/images/demosession/pexels-marta.jpg'
 function Demo() {
     return (
         <div className='demo'>
-            <div className='hero'>
-                <h2>Yoga Demo Session</h2>
-                <img src={image} alt="demo session" />
+            <div className='hero mb-2'>
+                <h2 className='mb-2'>Yoga Demo Session</h2>
+                <img src={image} alt="Yoga demo session" className='mb-2'/>
             </div>
             <div className='info'>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque ratione magnam iusto, explicabo cum aut. Ducimus eaque, porro commodi vitae officiis totam earum enim est blanditiis ratione? Eos, iure ipsam? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore quam reiciendis, perferendis recusandae facere officiis eligendi, laborum ipsa atque quis natus sequi alias rem velit, voluptatem mollitia aperiam? Tenetur, vero!</p>
+                <div className='info-p mb-2'>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit... Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio officiis nobis neque id quam laboriosam, saepe, eaque labore repellat ex dignissimos numquam qui. Eligendi blanditiis quas obcaecati officiis recusandae doloremque!</p>
+                    <div className='circle'></div>
+                </div>
                 <div className='book-info'>
-                    <h6>Availability</h6>
+                    <h6 className='mb-2'>Availability</h6>
                     <p>Monday - Friday</p>
                     <p>9:00am - 5:00pm</p>
                     <p>Eastern Time</p>
                     <p>Duration 1 hour</p>
-                    <button>Book Now</button>
+                    <button className='mb-2'>Book Now</button>
                 </div>
             </div>
         </div>
