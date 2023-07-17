ff336135c118d494f5c40d552162075dc17ad470
Louis Peter <louisclarencepeters@gmail.com>
2023-07-17 09:42:37 +0200

    added some style to the component

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -3,19 +3,19 @@ import image from '../assets/images/demosession/pexels-marta.jpg'
 function Demo() {
     return (
         <div className='demo'>
-            <div>
+            <div className='hero'>
                 <h2>Yoga Demo Session</h2>
                 <img src={image} alt="demo session" />
             </div>
             <div className='info'>
                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque ratione magnam iusto, explicabo cum aut. Ducimus eaque, porro commodi vitae officiis totam earum enim est blanditiis ratione? Eos, iure ipsam? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore quam reiciendis, perferendis recusandae facere officiis eligendi, laborum ipsa atque quis natus sequi alias rem velit, voluptatem mollitia aperiam? Tenetur, vero!</p>
-                <div>
+                <div className='book-info'>
                     <h6>Availability</h6>
                     <p>Monday - Friday</p>
                     <p>9:00am - 5:00pm</p>
                     <p>Eastern Time</p>
                     <p>Duration 1 hour</p>
-                    <button>Book a Demo Session</button>
+                    <button>Book Now</button>
                 </div>
             </div>
         </div>
