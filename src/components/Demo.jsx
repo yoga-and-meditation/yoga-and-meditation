1634155b7c52b6354b382fcaba274ca647687c2b
Louis Peter <louisclarencepeters@gmail.com>
2023-07-17 08:55:47 +0200

    Added more content to the main paragraph

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -8,7 +8,7 @@ function Demo() {
                 <img src={image} alt="demo session" />
             </div>
             <div>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque ratione magnam iusto, explicabo cum aut. Ducimus eaque, porro commodi vitae officiis totam earum enim est blanditiis ratione? Eos, iure ipsam?</p>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque ratione magnam iusto, explicabo cum aut. Ducimus eaque, porro commodi vitae officiis totam earum enim est blanditiis ratione? Eos, iure ipsam? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore quam reiciendis, perferendis recusandae facere officiis eligendi, laborum ipsa atque quis natus sequi alias rem velit, voluptatem mollitia aperiam? Tenetur, vero!</p>
                 <div>
                     <h6>Availability</h6>
                     <p>Monday - Friday</p>
