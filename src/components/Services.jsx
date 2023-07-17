319f322e4add7405b70d7ff96333e99cce2d895e
Abdulwase Naeemi <dci-admin@abdulwase-naeemi.local>
2023-07-17 14:28:31 +0200

    some new changes has been made

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -4,66 +4,8 @@ import image1 from "../assets/images/services/yoga.jpg";
 import image2 from "../assets/images/services/meditation.jpg";
 import image3 from "../assets/images/services/retreating.jpg";
 
-const servicesData = [
-  {
-    id: 1,
-    title: "Yoga",
-    image: image1,
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit.\n" +
-      "Veniam hic provident at unde porro odio voluptas repellat\n" +
-      "soluta! Ducimus voluptatem vero odio voluptatibus fugiat\n" +
-      "soluta asperiores, ullam reiciendis vel hic culpa\n" +
-      "architecto. Perferendis ducimus accusantium perspiciatis\n" +
-      "qui, accusamus libero earum laudantium maiores voluptatem\n" +
-      "officiis! Vel quos aliquam tenetur a possimus! asperiores,\n" +
-      "ullam reiciendis vel hic culpa architecto. Perferendis\n" +
-      "ducimus accusantium perspiciatis qui, accusamus libero earum\n" +
-      "laudantium maiores voluptatem officiis! Vel quos aliquam\n" +
-      "tenetur a possimus!",
-  },
-  {
-    id: 2,
-    title: "Meditation",
-    image: image2,
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit.\n" +
-      "Veniam hic provident at unde porro odio voluptas repellat\n" +
-      "soluta! Ducimus voluptatem vero odio voluptatibus fugiat\n" +
-      "soluta asperiores, ullam reiciendis vel hic culpa\n" +
-      "architecto. Perferendis ducimus accusantium perspiciatis\n" +
-      "qui, accusamus libero earum laudantium maiores voluptatem\n" +
-      "officiis! Vel quos aliquam tenetur a possimus! asperiores,\n" +
-      "ullam reiciendis vel hic culpa architecto. Perferendis\n" +
-      "ducimus accusantium perspiciatis qui, accusamus libero earum\n" +
-      "laudantium maiores voluptatem officiis! Vel quos aliquam\n" +
-      "tenetur a possimus!",
-  },
-  {
-    id: 3,
-    title: "Retreats",
-    image: image3,
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit.\n" +
-      "Veniam hic provident at unde porro odio voluptas repellat\n" +
-      "soluta! Ducimus voluptatem vero odio voluptatibus fugiat\n" +
-      "soluta asperiores, ullam reiciendis vel hic culpa\n" +
-      "architecto. Perferendis ducimus accusantium perspiciatis\n" +
-      "qui, accusamus libero earum laudantium maiores voluptatem\n" +
-      "officiis! Vel quos aliquam tenetur a possimus! asperiores,\n" +
-      "ullam reiciendis vel hic culpa architecto. Perferendis\n" +
-      "ducimus accusantium perspiciatis qui, accusamus libero earum\n" +
-      "laudantium maiores voluptatem officiis! Vel quos aliquam\n" +
-      "tenetur a possimus!",
-  },
-];
-
 function Services() {
   const [tab, setTab] = useState(1);
-
-  const handleTabClick = (id) => {
-    setTab(id);
-  };
 
   useEffect(() => {
     const list = document.querySelectorAll(".navigation li");
@@ -87,52 +29,137 @@ function Services() {
         <h3>Our Services</h3>
         <div className="tab-container">
           <ul className="navigation">
-            {servicesData.map((service) => (
-              <li
-                key={service.id}
-                className={tab === service.id ? "active" : ""}
-                onClick={() => handleTabClick(service.id)}
-              >
-                <button>
-                  <span className="icon">
-                    {tab === service.id ? (
-                      <ion-icon name="triangle-sharp"></ion-icon>
-                    ) : (
-                      <ion-icon name="person-outline"></ion-icon>
-                    )}
-                  </span>
-                  <span className="text">{service.title}</span>
-                </button>
-              </li>
-            ))}
+            <li className="active">
+              <button onClick={() => setTab(1)}>
+                <span className="icon">
+                  <ion-icon name="triangle-sharp"></ion-icon>
+                </span>
+                <span className="text">Yoga</span>
+              </button>
+            </li>
+            <li>
+              <button onClick={() => setTab(2)}>
+                <span className="icon">
+                  <ion-icon name="person-outline"></ion-icon>
+                </span>
+                <span className="text">Meditaion</span>
+              </button>
+            </li>
+            <li>
+              <button onClick={() => setTab(3)}>
+                <span className="icon">
+                  <ion-icon name="chatbubbles-outline"></ion-icon>
+                </span>
+                <span className="text">Retreats</span>
+              </button>
+            </li>
           </ul>
         </div>
       </div>
-      {servicesData.map((service) =>
-        tab === service.id ? (
-          <section key={service.id}>
-            <div className="card m-5 card-container">
-              <div className="row g-10">
-                <div className="col-md-4">
-                  <img
-                    src={service.image}
-                    className="img-fluid rounded-start"
-                    alt={`${service.title} image`}
-                  />
-                </div>
-                <div className="col-md-8">
-                  <div className="card-body">
-                    <h3 className="card-title">{service.title}</h3>
-                    <p className="card-text">{service.description}</p>
-                    <button className="subscribe-btn">
-                      Make an Appointment
-                    </button>
-                  </div>
+      {tab === 1 && (
+        <section>
+          <div className="card m-5 card-container">
+            <div className="row g-10">
+              <div className="col-md-4">
+                <img
+                  src={image1}
+                  className="img-fluid rounded-start"
+                  alt="Yoga image"
+                />
+              </div>
+              <div className="col-md-8">
+                <div className="card-body">
+                  <h3 className="card-title">Yoga</h3>
+                  <p className="card-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Veniam hic provident at unde porro odio voluptas repellat
+                    soluta! Ducimus voluptatem vero odio voluptatibus fugiat
+                    soluta asperiores, ullam reiciendis vel hic culpa
+                    architecto. Perferendis ducimus accusantium perspiciatis
+                    qui, accusamus libero earum laudantium maiores voluptatem
+                    officiis! Vel quos aliquam tenetur a possimus! asperiores,
+                    ullam reiciendis vel hic culpa architecto. Perferendis
+                    ducimus accusantium perspiciatis qui, accusamus libero earum
+                    laudantium maiores voluptatem officiis! Vel quos aliquam
+                    tenetur a possimus!
+                  </p>
+
+                  <button className="subscribe-btn">Make an Appointment</button>
                 </div>
               </div>
             </div>
-          </section>
-        ) : null
+          </div>
+        </section>
+      )}
+      {tab === 2 && (
+        <section>
+          <div className="card m-5 card-container">
+            <div className="row g-10">
+              <div className="col-md-4">
+                <img
+                  src={image2}
+                  className="img-fluid rounded-start"
+                  alt="Meditation image"
+                />
+              </div>
+              <div className="col-md-8">
+                <div className="card-body">
+                  <h3 className="card-title">Meditation</h3>
+                  <p className="card-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Veniam hic provident at unde porro odio voluptas repellat
+                    soluta! Ducimus voluptatem vero odio voluptatibus fugiat
+                    soluta asperiores, ullam reiciendis vel hic culpa
+                    architecto. Perferendis ducimus accusantium perspiciatis
+                    qui, accusamus libero earum laudantium maiores voluptatem
+                    officiis! Vel quos aliquam tenetur a possimus! asperiores,
+                    ullam reiciendis vel hic culpa architecto. Perferendis
+                    ducimus accusantium perspiciatis qui, accusamus libero earum
+                    laudantium maiores voluptatem officiis! Vel quos aliquam
+                    tenetur a possimus!
+                  </p>
+
+                  <button className="subscribe-btn">Make an Appointment</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      )}
+      {tab === 3 && (
+        <section>
+          <div className="card m-5 card-container">
+            <div className="row g-10">
+              <div className="col-md-4">
+                <img
+                  src={image3}
+                  className="img-fluid rounded-start"
+                  alt="Retreats image"
+                />
+              </div>
+              <div className="col-md-8">
+                <div className="card-body">
+                  <h3 className="card-title">Retreats</h3>
+                  <p className="card-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Veniam hic provident at unde porro odio voluptas repellat
+                    soluta! Ducimus voluptatem vero odio voluptatibus fugiat
+                    soluta asperiores, ullam reiciendis vel hic culpa
+                    architecto. Perferendis ducimus accusantium perspiciatis
+                    qui, accusamus libero earum laudantium maiores voluptatem
+                    officiis! Vel quos aliquam tenetur a possimus! asperiores,
+                    ullam reiciendis vel hic culpa architecto. Perferendis
+                    ducimus accusantium perspiciatis qui, accusamus libero earum
+                    laudantium maiores voluptatem officiis! Vel quos aliquam
+                    tenetur a possimus!
+                  </p>
+
+                  <button className="subscribe-btn">Make an Appointment</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
       )}
     </div>
   );
