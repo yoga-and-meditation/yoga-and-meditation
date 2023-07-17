900aac6c7df6e5281e3a9166b8cae7691d135c7c
Louis Peter <louisclarencepeters@gmail.com>
2023-07-17 09:21:13 +0200

    added more style to the headings

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -7,7 +7,7 @@ function Demo() {
                 <h2>Yoga Demo Session</h2>
                 <img src={image} alt="demo session" />
             </div>
-            <div>
+            <div className='info'>
                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque ratione magnam iusto, explicabo cum aut. Ducimus eaque, porro commodi vitae officiis totam earum enim est blanditiis ratione? Eos, iure ipsam? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore quam reiciendis, perferendis recusandae facere officiis eligendi, laborum ipsa atque quis natus sequi alias rem velit, voluptatem mollitia aperiam? Tenetur, vero!</p>
                 <div>
                     <h6>Availability</h6>
