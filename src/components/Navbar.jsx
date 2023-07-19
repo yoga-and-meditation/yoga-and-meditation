f79d1db406f34b759c5af7ff940a2667b08efe55
Shaqayeq-taheri <shaqayeq.taheritizro@dci.education>
2023-07-19 15:55:50 +0200

    navbar created from scratch

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,60 +1,39 @@
-import {Link ,Outlet} from 'react-router-dom'
+//import {Link ,Outlet} from 'react-router-dom'
+import { useState } from "react"; 
+import { FcMenu } from "react-icons/fc";
 
-import {
-  FcMenu} from "react-icons/fc";
 function Navbar() {
 
 
- 
+
+
+
 
   return (
-   
     <div>
-      <nav className="navbar navbar-expand-lg nav-background">
-        <div className="container-fluid">
-          <Link className="navbar-brand" to="">
-            Sheetal Chavan
-
-          </Link>
-         
-       <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-           
-
-          > <FcMenu/>
-            
-          </button> 
-         
-          <div className="collapse navbar-collapse justify-content-end gap-5" id="navbarSupportedContent">
-            <ul className="navbar-nav gap-2">
-              <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/hero">
-                  Home
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/aboutme">
-                About
-                </Link>
-              </li>
-
-              <li className="nav-item">
-                <Link className="nav-link" to="/services">Services</Link>
-              </li>
-            </ul>
-            <button className="btn" type="submit" >
-            <Link className="nav-link" to="/contacts">Contacts</Link> 
-            </button>
-          </div>
+      <nav className="navbar">
+        <div className="logo">
+          <h1>Sheetal Chavan</h1>
         </div>
+        <div>
+          <ul className="nav-menu">
+            <li>
+              <a href="#" />
+              Home
+            </li>
+            <li>
+              <a href="#" />
+              About
+            </li>
+            <li>
+              <a href="#" />
+              Services
+            </li>
+            <button>Contact</button>
+          </ul>
+        </div>
+        <div className="hamburger"><FcMenu onClick={handleClick}/></div>
       </nav>
-      <Outlet/>
     </div>
   );
 }
