a00923a78b3e02c771dc679a532d287159d13610
Louis Peter <louisclarencepeters@gmail.com>
2023-07-20 11:12:14 +0200

    fixed the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,67 +5,69 @@ import {
   FaLinkedin,
   FaEnvelope,
 } from "react-icons/fa";
-import logo from "../assets/images/footer-logo/Group.png"
+
 function Footer() {
   return (
     <footer>
       <div className="footer-container">
         <div className="logo-container">
-          <h4 className="logo">Yoga and Meditation <img src = {logo} alt="logo"  /></h4>
+          <h4 className="logo">Yoga and Meditation</h4>
         </div>
-        <div className=" about-me">
-          <h5>About me </h5>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
-            quidem pariatur autem neque, voluptatum sit error sint. Qui,
-            facilis. Consectetur temporibus ullam neque tempore, ut perferendis
-            nostrum necessitatibus repellendus fuga.
-          </p>
-          <div className="icons">
+        <div className="contact">
+          <div className=" about-me">
+            <h5>About me </h5>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
+              quidem pariatur autem neque, voluptatum sit error sint. Qui,
+              facilis. Consectetur temporibus ullam neque tempore, ut perferendis
+              nostrum necessitatibus repellendus fuga.
+            </p>
+            <div className="icons">
+              <ul>
+                <li className="list-group">
+                  <a href="https://www.facebook.com/sheetal.k.chavan">
+                    <FaFacebook />
+                  </a>
+                </li>
+                <li className="list-group">
+                  <a href="https://www.instagram.com/sheetalkchavan/">
+                    {" "}
+                    <FaInstagram />
+                  </a>
+                </li>
+                <li className="list-group">
+                  <a href="https://www.linkedin.com/in/sheetalchavan/">
+                    {" "}
+                    <FaLinkedin />
+                  </a>
+                </li>
+                <li className="list-group">
+                  <a href="website@sheetalchavan.com">
+                    {" "}
+                    <FaEnvelope />
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="  links">
+
             <ul>
+              <h5>Links</h5>
               <li className="list-group">
-                <a href="https://www.facebook.com/sheetal.k.chavan">
-                  <FaFacebook />
-                </a>
+                <a href="">Home</a>
               </li>
               <li className="list-group">
-                <a href="https://www.instagram.com/sheetalkchavan/">
-                  {" "}
-                  <FaInstagram />
-                </a>
+                <a href="#">About Me</a>
               </li>
               <li className="list-group">
-                <a href="https://www.linkedin.com/in/sheetalchavan/">
-                  {" "}
-                  <FaLinkedin />
-                </a>
+                <a href="#">Services</a>
               </li>
               <li className="list-group">
-                <a href="website@sheetalchavan.com">
-                  {" "}
-                  <FaEnvelope />
-                </a>
+                <a href="#">Contact</a>
               </li>
             </ul>
           </div>
-        </div>
-        <div className="  links">
-          
-          <ul>
-          <h5>Links</h5>
-            <li className="list-group">
-              <a href="">Home</a>
-            </li>
-            <li className="list-group">
-              <a href="#">About Me</a>
-            </li>
-            <li className="list-group">
-              <a href="#">Services</a>
-            </li>
-            <li className="list-group">
-              <a href="#">Contact</a>
-            </li>
-          </ul>
         </div>
       </div>
       <div className="copyright p-3 fixed-bottom">
