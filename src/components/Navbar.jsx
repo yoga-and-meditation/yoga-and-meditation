49b728f147417970946e986252a01f2e8242dce8
Roshini Muralidharan <Roshinipraveen18@gmail.com>
2023-07-20 11:54:21 +0200

    responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,38 +1,39 @@
 //import {Link ,Outlet} from 'react-router-dom'
-import { useState } from "react"; 
-import { FcMenu } from "react-icons/fc";
-
+ import { useState } from "react"; 
+ import { FcMenu } from "react-icons/fc";
+ import { GrClose } from "react-icons/gr";
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
 
-
-
-
+  const toggleNav = () => {setIsOpen(!isOpen)};
 
 
   return (
     <div>
-      <nav className="navbar">
+      <nav className={isOpen? 'navbar expanded':'navbar'}>
         <div className="logo">
           <h1>Sheetal Chavan</h1>
         </div>
         <div>
           <ul className="nav-menu">
-            <li>
+            <li className="item">
               <a href="#" />
               Home
             </li>
-            <li>
+            <li className="item">
               <a href="#" />
               About
             </li>
-            <li>
+            <li className="item">
               <a href="#" />
               Services
-            </li>
-            <button>Contact</button>
-          </ul>
+            </li >
+            <button className="item">Contact</button>
+          </ul> 
         </div>
-        <div className="hamburger"><FcMenu onClick={handleClick}/></div>
+        <div className="hamburger" onClick={toggleNav}>
+          {isOpen?<GrClose/>:<FcMenu/>}
+        </div> 
       </nav>
     </div>
   );
