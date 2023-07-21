ec3562e39304fd038f28807d42341611943c0b25
Louis Peter <louisclarencepeters@gmail.com>
2023-07-21 10:30:47 +0200

    router and routers removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/App.scss";
 import Aboutme from "./components/Aboutme";
