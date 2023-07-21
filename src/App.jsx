5b053f483966c48720a7322e1db5aac4bafd8a30
Abdulwase Naeemi <dci-admin@abdulwase-naeemi.local>
2023-07-21 09:24:39 +0200

    bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,29 @@
+/* import { BrowserRouter, Routes, Route } from "react-router-dom"; */
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/App.scss";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Aboutme from './components/Aboutme'
-import Services from './components/Services'
-import Hero from './components/Hero'
+import Aboutme from "./components/Aboutme";
+import Services from "./components/Services";
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Contacts from "./components/Contacts";
+import Demo from "./components/Demo";
+import Gallery from "./components/Gallery";
+import Feedback from "./components/Feedback";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <> 
-    <Navbar/>
-    <Hero/>
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Navbar/>}>
-        <Route path="/aboutme" element={<Aboutme/>} />
-        <Route path="/services" element={<Services/>} />
-        <Route path="/hero" element={<Hero/>} />
-        <Route path="/contacts" element={<Contacts/>} />
-
-      </Route>
-    </Routes>
-  </BrowserRouter></>
-    
-  
+    <>
+      <Navbar />
+      <Hero />
+      <Aboutme />
+      <Services />
+      <Demo />
+      <Gallery />
+      <Feedback />
+      <Contacts />
+      <Footer />
+    </>
   );
 }
 
