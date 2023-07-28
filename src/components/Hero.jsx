f21768e5ca2c3a0acd4f58ea0e92b110bae9bafc
Abdulwase Naeemi <dci-admin@abdulwase-naeemi.local>
2023-07-28 18:36:45 +0200

    hero component has been fixed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,10 +2,9 @@ import name from "../assets/images/name.png";
 import gif1 from "../assets/images/gif1.png";
 function Hero() {
   return (
-    <div>
-      <div className="hero-container"></div>
-      <div>
-        <h1 className="hero-heading">Yoga</h1>
+    <div className="hero-container">
+      <div className="hero-heading">
+        <h1>Yoga</h1>
         <img src={name} className="name" />
       </div>
       <div className="ball">
