615acc3ee80daeea94fdaae108c96e87c4a502dc
Abdulwase Naeemi <dci-admin@abdulwase-naeemi.local>
2023-07-28 16:38:42 +0200

    some useful comment has been added to app.scss

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,18 @@
-import name from '../assets/images/name.png'
-import gif1 from '../assets/images/gif1.png'
+import name from "../assets/images/name.png";
+import gif1 from "../assets/images/gif1.png";
 function Hero() {
-    return (
-<div >
-  <div className='hero' ></div>
-      <div >
-       <h1 className='hero-heading'>Yoga</h1>
-       <img src={name} className="name"/></div>
-       <div className='ball'><img src={gif1} className='gif'></img>
-     </div>
- 
+  return (
+    <div>
+      <div className="hero-container"></div>
+      <div>
+        <h1 className="hero-heading">Yoga</h1>
+        <img src={name} className="name" />
       </div>
-    );
-    
-  }
-  
-  export default Hero;
+      <div className="ball">
+        <img src={gif1} className="gif"></img>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
