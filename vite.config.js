3751313a94ab925f6aba3009bcfb5a879dd13bdd
Abdulwase Naeemi <dci-admin@abdulwase-naeemi.local>
2023-07-31 15:48:44 +0200

    bug fixed

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,9 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import dynamicImportVars from "@rollup/plugin-dynamic-import-vars";
+import react from "@vitejs/plugin-react";
 
-//Configuration for Vite
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    //Use the Vite plugin for react with SWC (Speedy Web Compiler)
-    react(),
-
-    //Use the Rollup plugin to handle dynamic imports with variables
-    dynamicImportVars({
-      // options
-    }),
-  ],
-
+  plugins: [react()],
   server: {
     port: 3000,
   },
