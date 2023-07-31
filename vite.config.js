c66140b81c1f8d26dde3fce1bdff0a2c8f443db9
Abdulwase Naeemi <dci-admin@abdulwase-naeemi.local>
2023-07-31 14:50:16 +0200

    the Rollup plugin to handle dynamic imports with variables has been aded to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import dynamicImportVars from "@rollup/plugin-dynamic-import-vars";
 
+//Configuration for Vite
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    //Use the Vite plugin for react with SWC (Speedy Web Compiler)
+    react(),
+
+    //Use the Rollup plugin to handle dynamic imports with variables
+    dynamicImportVars({
+      // options
+    }),
+  ],
+
   server: {
     port: 3000,
   },
