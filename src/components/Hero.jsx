4cb68db71b6a81abd7896de04405b019e54bdaf2
Roshini Muralidharan <Roshinipraveen18@gmail.com>
2023-08-08 09:33:22 +0200

    added media query

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,11 @@
-import name from "../assets/images/name.png";
 import gif1 from "../assets/images/gif1.png";
 function Hero() {
   return (
     <div className="hero-container">
       <div className="hero-heading">
         <h1>Yoga</h1>
-        <img src={name} className="name" />
+        {/* <img src={name} className="name" /> */}
+        <h2>With Sheetal</h2>
       </div>
       <div className="ball">
         <img src={gif1} className="gif"></img>
