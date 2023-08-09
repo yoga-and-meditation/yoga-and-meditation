f89ae67b7f3be6fb2d92eef82613dc042e2462f1
Abdulwase Naeemi <dci-admin@abdulwase-naeemi.local>
2023-08-09 22:51:11 +0200

    some changes has been made to the aboutme section as a trial

--- src/components/Aboutme.jsx
+++ src/components/Aboutme.jsx
@@ -3,17 +3,22 @@ import sheetalImg from "../assets/images/My project-1.png";
 function Aboutme() {
   return (
     <div className="about-container">
-      <img className="about-image" src={sheetalImg} alt="personal photo" />
-      <div className="rectangle1"></div>
-      <div className="rectangle2"></div>
+      <div className="image-container">
+        <img className="about-image" src={sheetalImg} alt="personal photo" />
+        <div className="rectangle1"></div>
+        <div className="rectangle2"></div>
+      </div>
 
-      <section>
+      <section className="content-container">
         <h2>About Me</h2>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
           animi voluptatum voluptatem, facilis, fugiat quam placeat consequatur
           id eveniet pariatur labore, quo quia fuga dignissimos perspiciatis.
-          Eius fuga delectus recusandae.
+          Eius fuga delectus recusandae. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Sapiente animi voluptatum voluptatem, facilis,
+          fugiat quam placeat consequatur id eveniet pariatur labore, quo quia
+          fuga dignissimos perspiciatis. Eius fuga delectus recusandae.
         </p>
       </section>
     </div>
