1cc74cb2ad1d7ec0fdd95af685b0aa1eaddbbaa3
Abdulwase Naeemi <dci-admin@abdulwase-naeemi.local>
2023-08-15 14:07:41 +0200

    the background image has been blured

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,31 @@
 import Navbar from "../components/Navbar";
 function Hero() {
-  return (<><Navbar/>
-    <div className="hero-container bg">
-    <div className="bgc"></div>
+  return (
+    <>
+      <Navbar />
+      <div className="hero-container bg">
+        <div className="bgc"></div>
 
-     <div className="content"> <h2>SHEETAL CHAVAN</h2>
-      <h1>TAKE CHARGE OF YOUR WELL-BEING</h1>
-     <p>In this ever-demanding, uncertain and ever-changing world full of distractions, be the one to take charge of your well-being. Join our meditation courses/ workshops and yoga sessions for calming the mind and body, managing the stress, anxiety, increased clarity and developing a still and a focussed mind with ancient techniques for daily self-practice.</p>
-  <button><span className="icon"><i className="fa-solid fa-angle-down"></i></span></button>
-  </div>
-  </div></>
+        <div className="content">
+          {" "}
+          <h2>SHEETAL CHAVAN</h2>
+          <h1>TAKE CHARGE OF YOUR WELL-BEING</h1>
+          <p>
+            In this ever-demanding, uncertain and ever-changing world full of
+            distractions, be the one to take charge of your well-being. Join our
+            meditation courses/ workshops and yoga sessions for calming the mind
+            and body, managing the stress, anxiety, increased clarity and
+            developing a still and a focussed mind with ancient techniques for
+            daily self-practice.
+          </p>
+          <button>
+            <span className="icon">
+              <i className="fa-solid fa-angle-down"></i>
+            </span>
+          </button>
+        </div>
+      </div>
+    </>
   );
 }
 
