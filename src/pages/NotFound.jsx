039cd900b20dff26a6d86c9709ede3e0af7fe9d1
Abdulwase Naeemi <dci-admin@abdulwase-naeemi.local>
2023-08-16 17:31:19 +0200

    the under-construction page has been modified

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,4 +1,4 @@
-//import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import {
   FaFacebook,
   FaInstagram,
@@ -7,21 +7,21 @@ import {
 } from "react-icons/fa";
 
 function NotFound() {
-  //const [percentage, setPercentage] = useState(0);
+  const [percentage, setPercentage] = useState(0);
 
   {
-    /*useEffect(() => {
-    const progressLoad = () => {
-      if (percentage >= 70) {
-        setPercentage(70);
-      } else {
-        setPercentage((prevPercentage) => prevPercentage + 1);
-      }
-    };
+    useEffect(() => {
+      const progressLoad = () => {
+        if (percentage >= 70) {
+          setPercentage(70);
+        } else {
+          setPercentage((prevPercentage) => prevPercentage + 1);
+        }
+      };
 
-    const interval = setInterval(progressLoad, 90);
-    return () => clearInterval(interval);
-  }, [percentage]);*/
+      const interval = setInterval(progressLoad, 90);
+      return () => clearInterval(interval);
+    }, [percentage]);
   }
 
   return (
@@ -33,44 +33,39 @@ function NotFound() {
           <h2>Yoga and Meditation</h2>
           <hr />
         </div>
-        <h1>COMING SOON</h1>
-        {/*<h3>PROGRESS</h3>
+
+        <h3>PROGRESS</h3>
         <div className="progress-wrapper">
           <div className="progress" style={{ width: `${percentage}%` }}>
             <span>{`${percentage}%`}</span>
           </div>
-        </div>*/}
+        </div>
 
         <p>Please visit us back on 17th of August</p>
-        <button>
-          <a href="\docs\info.pdf" download>
-            More Info
-          </a>
-        </button>
 
         <div className="social-icons">
           <a
-            href="https://www.facebook.com/sheetal.k.chavan"
+            href="https://www.facebook.com"
             target="_blank"
             rel="noopener noreferrer"
           >
             <FaFacebook />
           </a>
           <a
-            href="https://www.instagram.com/sheetalkchavan/"
+            href="https://www.instagram.com"
             target="_blank"
             rel="noopener noreferrer" /*    rel="noopener noreferrer" attribute, which is a recommended security practice when using target="_blank". This helps prevent potential security vulnerabilities when opening links in a new tab.  */
           >
             <FaInstagram />
           </a>
           <a
-            href="https://www.linkedin.com/in/sheetalchavan/"
+            href="https://www.linkedin.com"
             target="_blank"
             rel="noopener noreferrer"
           >
             <FaLinkedin />
           </a>
-          <a href="mailto:info@sheetalchavan.com">
+          <a href="mailto:thebestteam.dci@gmail.com">
             <FaEnvelope />
           </a>
         </div>
