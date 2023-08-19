a74a4f2c8839b687e7f39c9eb5d3315a90c757ef
Abdulwase Naeemi <abdulwasenaeemi7@gmail.com>
2023-08-19 17:21:46 +0200

    the design for the service component has been changed

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -30,16 +30,16 @@ const cardsData = [
   [
     // Courses under Yoga service
     {
-      header: "Free trail",
+      header: "Trial",
       price: "NIA",
-      buttonText: "Contact now",
+      buttonText: "Book now",
       className: "basic",
       elements: ["Corporate Meditation,", "Yoga", "&", "Retreats workshops"],
     },
     {
-      header: "Online Yoga",
+      header: "Daily",
       price: "€8 /session",
-      buttonText: "Join now",
+      buttonText: "Book now",
       className: "standard",
       elements: [
         "1 session of 70 minutes",
@@ -49,9 +49,9 @@ const cardsData = [
       ],
     },
     {
-      header: "In-person Yoga",
+      header: "Monthly",
       price: "€13 /session",
-      buttonText: "Join now",
+      buttonText: "Book now",
       className: "premium",
       elements: [
         "1 session of 1.5 hours",
@@ -68,7 +68,7 @@ const cardsData = [
     {
       header: "Online Meditation",
       price: "€70 /course",
-      buttonText: "Join now",
+      buttonText: "Book now",
       className: "standard",
       elements: [
         "4 sessions of 45 minutes",
@@ -80,7 +80,7 @@ const cardsData = [
     {
       header: "In-person Meditation",
       price: "€80 /course",
-      buttonText: "Join now",
+      buttonText: "Book now",
       className: "premium",
       elements: [
         "2 sessions of 1.5 hours",
@@ -103,7 +103,7 @@ const cardsData = [
     {
       header: "Online Meditation",
       price: "€70 /course",
-      buttonText: "Join now",
+      buttonText: "Book now",
       className: "standard",
       elements: [
         "4 sessions of 45 minutes",
@@ -115,7 +115,7 @@ const cardsData = [
     {
       header: "In-person Meditation",
       price: "€80 /course",
-      buttonText: "Join now",
+      buttonText: "Book now",
       className: "premium",
       elements: [
         "2 sessions of 1.5 hours",
@@ -127,7 +127,7 @@ const cardsData = [
     {
       header: "Coming soon",
       price: "NIA",
-      buttonText: "Contact now",
+      buttonText: "Book now",
       className: "basic",
       elements: ["Corporate Meditation,", "Yoga", "&", "Retreats workshops"],
     },
@@ -143,73 +143,74 @@ function Services() {
 
   return (
     <div className="service-container">
-      <div className="header-container">
-        <h2>What We Do?</h2>
-        <h3>Our Services</h3>
-      </div>
-
-      <div className="tab-container">
-        <ul className="navigation">
-          {serviceData.map((service, index) => (
-            <li
-              key={index}
-              className={tab === index ? "active" : ""}
-              onClick={() => handleTabClick(index)}
-            >
-              <div className="button">
-                <span className="icon">
-                  <img
-                    src={
-                      tab === index ? service.activeIcon : service.inActiveIcon
-                    }
-                    alt={`${service.title} icon`}
-                  />
-                </span>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </div>
-
-      <section className="service-section">
-        <div className="card-content-container">
-          <div className="cards-container">
-            {cardsData[tab].map((content, index) => (
-              <div key={index} className={`card-${content.className}`}>
-                {/* Card Header */}
-                <div className={`card-header header-${content.className}`}>
-                  <h3>{content.header}</h3>
+      <div className="service-wrapper">
+        <div className="header-container">
+          <h2>Our Services</h2>
+        </div>
+        <div className="tab-container">
+          <ul className="navigation">
+            {serviceData.map((service, index) => (
+              <li
+                key={index}
+                className={tab === index ? "active" : ""}
+                onClick={() => handleTabClick(index)}
+              >
+                <div className="button">
+                  <span className="icon">
+                    <img
+                      src={
+                        tab === index
+                          ? service.activeIcon
+                          : service.inActiveIcon
+                      }
+                      alt={`${service.title} icon`}
+                    />
+                  </span>
                 </div>
-                {/* Card Body */}
-                <div className="card-body">
-                  {content.price && (
-                    <p>
-                      <h3>{content.price}</h3>
-                    </p>
-                  )}
-                  <div className={`card-element-hidden-${content.className}`}>
-                    {/* List of elements for the course */}
-                    <ul className="card-element-container">
-                      {content.elements.map((element, elementIndex) => (
-                        <li className="card-element" key={elementIndex}>
-                          {element}
-                        </li>
-                      ))}
-                    </ul>
-                    {/* Button to join the course */}
-                    <Link
-                      to="/contacts"
-                      className={`btn btn-${content.className}`}
-                    >
-                      {content.buttonText}
-                    </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <section className="service-section">
+          <div className="card-content-container">
+            <div className="cards-container">
+              {cardsData[tab].map((content, index) => (
+                <div key={index} className={`card-${content.className}`}>
+                  {/* Card Header */}
+                  <div className={`card-header header-${content.className}`}>
+                    <h3>{content.header}</h3>
+                  </div>
+                  {/* Card Body */}
+                  <div className="card-body">
+                    {content.price && (
+                      <p>
+                        <h3>{content.price}</h3>
+                      </p>
+                    )}
+                    <div className={`card-element-hidden-${content.className}`}>
+                      {/* List of elements for the course */}
+                      <ul className="card-element-container">
+                        {content.elements.map((element, elementIndex) => (
+                          <li className="card-element" key={elementIndex}>
+                            {element}
+                          </li>
+                        ))}
+                      </ul>
+                      {/* Button to join the course */}
+                      <Link
+                        to="/contacts"
+                        className={`btn btn-${content.className}`}
+                      >
+                        {content.buttonText}
+                      </Link>
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </div>
     </div>
   );
 }
