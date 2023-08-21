0cf654de6dd6821d21445b394629201207921451
Roshini Muralidharan <Roshinipraveen18@gmail.com>
2023-08-21 14:00:18 +0200

    not needed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import { BrowserRouter } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
+/* import { BrowserRouter } from "react-router-dom";
+ */import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/App.scss";
-import Aboutme from "./components/Aboutme";
+import Signup from "./components/Signup";
+/* import Aboutme from "./components/Aboutme";
 import Services from "./components/Services";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
@@ -9,12 +10,13 @@ import Contacts from "./components/Contacts";
 import Demo from "./components/Demo";
 import Gallery from "./components/Gallery";
 import Feedback from "./components/Feedback";
-import Footer from "./components/Footer";
+import Footer from "./components/Footer"; */
 
 function App() {
   return (
     <div className="app">
-      <BrowserRouter>
+      <Signup/>
+     {/*  <BrowserRouter>
         <Navbar />
         <Hero />
         <Aboutme />
@@ -24,7 +26,7 @@ function App() {
         <Demo />
         <Contacts />
         <Footer />
-      </BrowserRouter>
+      </BrowserRouter> */}
     </div>
   );
 }
