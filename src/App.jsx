d527efac27f5c8a1a25b44003136db6b659d0e94
Abdulwase Naeemi <abdulwasenaeemi7@gmail.com>
2023-08-22 22:24:31 +0200

    signin and signup components has been created using tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,11 @@
-import { BrowserRouter } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./styles/App.scss";
-import Aboutme from "./components/Aboutme";
-import Services from "./components/Services";
-import Hero from "./components/Hero";
-import Navbar from "./components/Navbar";
-import Contacts from "./components/Contacts";
-import Demo from "./components/Demo";
-import Gallery from "./components/Gallery";
-import Feedback from "./components/Feedback";
-import Footer from "./components/Footer";
 import Signin from "./components/Signin";
 import Signup from "./components/Signup";
 
 function App() {
   return (
     <div className="app">
-      <BrowserRouter>
-        <Signin />
-        <Signup />
-        {/* <Navbar />
-        <Hero />
-        <Aboutme />
-        <Services />
-        <Gallery />
-        <Feedback />
-        <Demo />
-        <Contacts />
-        <Footer /> */}
-      </BrowserRouter>
+      <Signin />
+      {/* <Signup /> */}
     </div>
   );
 }
