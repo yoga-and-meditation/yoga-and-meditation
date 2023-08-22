bb2932e7f0acf0abc9463731f0e222e62137c4d7
Abdulwase Naeemi <abdulwasenaeemi7@gmail.com>
2023-08-22 22:28:18 +0200

    bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-import Signin from "./components/Signin";
-import Signup from "./components/Signup";
+import { BrowserRouter } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/App.scss";
+import Aboutme from "./components/Aboutme";
+import Services from "./components/Services";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Contacts from "./components/Contacts";
+import Demo from "./components/Demo";
+import Gallery from "./components/Gallery";
+import Feedback from "./components/Feedback";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="app">
-      <Signin />
-      {/* <Signup /> */}
+      <BrowserRouter>
+        <Navbar />
+        <Hero />
+        <Aboutme />
+        <Services />
+        <Gallery />
+        <Feedback />
+        <Demo />
+        <Contacts />
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
