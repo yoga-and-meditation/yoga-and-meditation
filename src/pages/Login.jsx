505c97e56aafcd0cee46bb809ae719ba0a0970bd
Abdulwase Naeemi <abdulwasenaeemi7@gmail.com>
2023-08-26 17:42:17 +0200

    the barder of the signin and signup containers has been removed

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -36,7 +36,7 @@ function Login(props) {
     <>
       <section className="bg-white signin-container">
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-          <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 ">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-dark text-center">
                 Sign in to your account
