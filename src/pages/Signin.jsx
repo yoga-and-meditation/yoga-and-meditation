17cedf00313ed48ebd682aa110e3d0b7fa6654aa
Abdulwase Naeemi <abdulwasenaeemi7@gmail.com>
2023-08-26 12:19:09 +0200

    bug fiex

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -15,7 +15,7 @@ function Signin(props) {
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
           <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 className="text-xl font-bold leading-tight tracking-tight text-black md:text-2xl dark:text-white">
+              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-dark text-center">
                 Sign in to your account
               </h1>
               <form
