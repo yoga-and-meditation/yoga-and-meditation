c742f4d27af02bd5a23d2cfc361327bd168791e8
Louis Peter <louisclarencepeters@gmail.com>
2023-09-04 21:09:58 +0200

    added an inview animation but it is not working.

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,3 +1,5 @@
+import { useInView } from 'react-intersection-observer';
+
 function BookingInfo({ availability, time, timezone, duration }) {
   return (
     <div className="booking-info-container">
@@ -10,7 +12,9 @@ function BookingInfo({ availability, time, timezone, duration }) {
         <li>{timezone}</li>
         <li>Duration {duration}</li>
       </ul>
-      <button className="mb-2" aria-label="Book Yoga Session Now">Book Now</button>
+      <button className="mb-2" aria-label="Book Yoga Session Now">
+        Book Now
+      </button>
     </div>
   );
 }
@@ -21,16 +25,26 @@ function Demo() {
   const timezone = "Eastern Time";
   const duration = "1 hour";
 
+  const [inViewRef, inView] = useInView({
+    triggerOnce: true,
+  });
+
+  const animationStyle = inView
+    ? {
+        animation: "fade-in 0.8s 0.5s forwards cubic-bezier(0.11, 0, 0.5, 0)",
+      }
+    : {};
+
+  console.log("InView:", inView); // Check the inView status in the console.
+
   return (
     <div className="demo">
       <h2 className="mb-2">Yoga Demo Session</h2>
       <div className="demo-container">
-
-        <div className="demo-session-description">
+        <div className="demo-session-description" ref={inViewRef} style={animationStyle}>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui.
           </p>
-
           <BookingInfo
             availability={availability}
             time={time}
