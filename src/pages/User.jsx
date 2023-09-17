1b42feb76600afefc7d3d82e469e8118006c2a81
Abdulwase Naeemi <abdulwasenaeemi7@gmail.com>
2023-09-17 23:05:56 +0200

    bug fixed

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,7 +1,15 @@
 function user() {
   return (
-    <div>
-      <h1>Welcome</h1>
+    <div className="user-profile">
+      <h1 className="card-title text-left">Welcome!</h1>
+      <p className="card-text">
+        You logged in to your page, You will receive a confirmation soon!
+      </p>
+      <button className="user-btn">
+        <a href="#" className="btn">
+          Go Home
+        </a>
+      </button>
     </div>
   );
 }
